45c96c28f6313b671ba55d7e671fc66b306fa4dc
yukachilov <60588319+yukachilov@users.noreply.github.com>
2020-02-13 19:29:45 +0900

    config2

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,8 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.12.0'
 
+set :linked_files, %w{ config/secrets.yml }
+
 # Capistranoのログの表示に利用する
 set :application, 'chat-space'
 
@@ -26,9 +28,22 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
