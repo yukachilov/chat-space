edabe41b1b69b64a3f9121d4676bb07c2beae394
yukachilov <60588319+yukachilov@users.noreply.github.com>
2020-02-13 17:32:50 +0900

    unicorn

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.176.137.126, user': 'ec2-user', roles: %w{app db web}
+server '18.176.137.126', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
